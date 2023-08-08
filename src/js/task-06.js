5a6816d61c36b9d4cb75575437bf1a6755d1c63b
Juliya Shamray <fizmatka908990@gmail.com>
2023-08-08 21:30:14 +0300

    event change

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,7 +2,7 @@
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 
 const validateInput = document.querySelector('#validation-input');
-validateInput.addEventListener('input', onSymbolInput);
+validateInput.addEventListener('change', onSymbolInput);
 
 function onSymbolInput() {
   validateInput.classList.add('valid');
